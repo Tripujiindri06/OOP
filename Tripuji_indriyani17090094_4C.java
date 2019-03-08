f55ac1765713f59d155c10d7f0e8d5d43da50794
Tripujiindri06 <48343815+Tripujiindri06@users.noreply.github.com>
2019-03-08 13:20:21 +0700

    Create Tripuji_indriyani17090094_4C.java

---
+++ Tripuji_indriyani17090094_4C.java
@@ -0,0 +1,7 @@
+package tugas;
+
+public class datang {
+    public static void main(String[] args){
+        system.out.println("Selamat datang di Object Oriented Programming \n Tripuji_indriyani_17090094_4C");
+    }
+}
